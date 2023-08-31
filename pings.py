ac1260bf00699e3b0c6588964a5c1e4be2149752
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-08-31 17:02:11 +0100

    Add files via upload

--- pings.py
+++ pings.py
@@ -7,11 +7,11 @@ def ping(host):
         start_time = time.time()
         result = subprocess.run(['ping', host,'-n', '1'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=3)
         end_time = time.time()
-        
+        output = result.stdout.decode()
         if result.returncode == 0:
-            print(f"Resposta recebida do host {host} em {end_time - start_time:.4f} segundos.")
+            print(f"{output}")
         else:
-            print(f"Não foi possível receber resposta do host {host}.")
+            print(f"{output}")
     except subprocess.TimeoutExpired:
         print(f"Tempo limite de 2 segundos excedido para o host {host}.")
 print("\033c\033[47;34m")
